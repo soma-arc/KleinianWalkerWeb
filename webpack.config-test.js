a9aacf012aaeba90618f3139ee8cc6f7bdd26f0f
soma_arc <somaarcr@gmail.com>
2017-03-30 16:57:14 +0900

    Remove unused package

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -4,7 +4,7 @@ const src = path.resolve(__dirname, 'test');
 const dist = path.resolve(__dirname, 'tmp');
 
 module.exports = {
-    entry: src + '/test_index.js',
+    entry: `${src}/index.js`,
     module: {
         loaders: [
             {
